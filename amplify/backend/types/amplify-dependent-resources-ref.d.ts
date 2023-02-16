8e4d8b995164234f2b3aa3bf16587f99bb0a6203
Abdallah Shaban <abdallah.w.shaban@gmail.com>
2023-02-15 16:36:01 -0800

    updated and added hosting

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,20 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "function": {
+    "S3Triggercb8e53ca": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "s349b5afe9": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
