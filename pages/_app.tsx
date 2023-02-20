e75e0aff1abea2a3ceb45b0dffa6aede534153c4
Abdallah Shaban <abdallah.w.shaban@gmail.com>
2023-02-19 19:57:08 -0800

    updated config for theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Amplify, Auth } from 'aws-amplify';
-import awsconfig from '../src/aws-exports';
-
-Amplify.configure({Auth: {identityPoolId: ''}});
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Amplify, Auth } from "aws-amplify";
+import awsconfig from "../src/aws-exports";
+import { ThemeProvider } from "@aws-amplify/ui-react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
